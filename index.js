ae413c73031ad4fdc0165776427055d874ec6738
Joan in DC <joanrigdon@gmail.com>
2019-01-15 01:59:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -66,8 +66,9 @@ function placeOrder(cardNumber) {
   if (!cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   }
+  cart.splice(0,cart.length);
+  
   if (cardNumber) {
-    
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`}
   }
 }

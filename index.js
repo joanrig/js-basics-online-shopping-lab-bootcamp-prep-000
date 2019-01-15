ab33919d702b03a18ca7ebad5c1fb557b4bcefd9
Joan in DC <joanrigdon@gmail.com>
2019-01-15 01:52:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -67,8 +67,8 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   }
   if (cardNumber) {
-    for (let i = 0; i < cart.length; i++ ) {
-      cart[i].pop
+    for (let i = cart.length) {
+       cart.pop
     }
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
   }

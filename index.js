2f5d00bfedef23a93d5af604ba7128ec47e4ce33
Joan in DC <joanrigdon@gmail.com>
2019-01-15 01:34:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -61,5 +61,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (!cardNumber)
+    return "Sorry, we don't hve a credit card on file for you."
+  
 }

4bb2c7f91b7a95e267dc3148065641f1a85d9e1e
Joan in DC <joanrigdon@gmail.com>
2019-01-15 02:12:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -63,13 +63,12 @@ function removeFromCart(item) {
 
 
 function placeOrder(cardNumber) {
-  if (!cardNumber) {
-    return "Sorry, we don't have a credit card on file for you."
-  }
+
   cart.slice(0,cart.length);
   
   if (cardNumber) {
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
-    
+  } else {
+    return "Sorry, we don't have a credit card on file for you."
   }
 }
